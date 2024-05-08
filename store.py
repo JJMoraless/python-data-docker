75f778348668f17b0d50290020a0ea840ed71885
JJMoraless <jjmorales.dev@gmail.com>
2024-05-07 20:26:14 -0500

    add docker

--- store.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_categories():
-    res_categories = requests.get("https://api.escuelajs.co/api/v1/categories")
-    categories = res_categories.json()
-    return categories
-
